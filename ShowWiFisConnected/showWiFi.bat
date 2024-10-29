cbc3c7d49acf4d7e4f7a935fc942ffa4e5d6cb88
KhanhEK2846 <lehoangkhanh2846912002@gmail.com>
2024-10-29 19:41:15 +0700

    Add show Wifi - Show wifi password that was connected

---
+++ ShowWiFisConnected/showWiFi.bat
@@ -0,0 +1,16 @@
+echo OFF
+
+del profile.txt
+
+for /f "tokens=4,* skip=4" %%a in ('netsh wlan show profile') do (
+
+    for /f "tokens=*" %%c in ('netsh wlan show profile "%%b" key^=clear') do (
+
+        for /f "tokens=3,*" %%d in ('echo %%c^| find /i "Key Content"') do (
+
+            echo [WIFI-NAME: %%b] [CURRENT-PASSWORD: %%e] >> profile.txt
+
+        )
+    )
+)
+
